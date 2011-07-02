bdf57fdeaef33602ba8371b5bafdf5796e5f31c9
Nopticon <nopticon@gmail.com>
2011-07-01 23:46:56 -0600

    Conn

--- conexion.php
+++ conexion.php
@@ -1,15 +1,10 @@
 <?php
-/*
-$usuario = 'mipeten';
-$contrasena ='m1p3t3n';
-$db = 'mipeten_edupro';
-*/
+
 $usuario = 'root';
 $contrasena ='sistemapeten';
 $db = 'edupro';
 
 $servidor = 'localhost';
-//$servidor = 'mysql.mipeten.com';
 
 $conectar = mysql_connect($servidor, $usuario, $contrasena) or die ('No se pudo conectar al servidor porque: ' . mysql_error());
 $seleccionar = mysql_select_db($db) or die ('No se pudo seleccionar la base de datos porque: ' . mysql_error());
@@ -18,10 +13,10 @@ session_start();
 
 function encabezado($titulo = '', $ruta = '', $full = true)
 {
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+?><!DOCTYPE HTML>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta charset="iso-8859-1" />
 <title><?php echo $titulo; ?></title>
 <link rel="stylesheet" type="text/css" href="../<?php echo $ruta; ?>style.css" />
 <script src="../<?php echo $ruta; ?>ff.js" type="text/javascript"></script>
@@ -34,15 +29,14 @@ function encabezado($titulo = '', $ruta = '', $full = true)
 
 <?php
 
-if ($full)
-{
-	?>
-<div id="header">
-	<img src="../<?php echo $ruta; ?>images/normal.jpg" width="830" height="150" alt="" />
-</div>
-	<?php
-}
-
+	if ($full)
+	{
+		?>
+	<div id="header">
+		<img src="../<?php echo $ruta; ?>images/normal.jpg" width="830" height="150" alt="" />
+	</div>
+		<?php
+	}
 }
 
 ?>
